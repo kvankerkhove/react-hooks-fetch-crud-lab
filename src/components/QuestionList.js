404ff243fa990df74a3f5767f3ab517890aec86e
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-02 12:44:00 -0400

    commit tree a26e26e48cdbd43f9711622ad10dfe35e6971467

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, handleDelete}) {
+  const questionItems = questions.map((question) => {
+    return <QuestionItem key={question.id} handleDelete= {handleDelete} question={question}/>
+  })
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
